30b3ddc8548519e0cc051ee706f4d8c2bba10d5f
Enkhbaatar Idersaikhan <roberthdoll@gmail.com>
2025-03-20 21:04:43 +0000

    Added ShapeTest;

---
+++ WorksheetThree_3112121/src/griffith/ShapeTest.java
@@ -0,0 +1,26 @@
+//Enkhbaatar Idersaikhan 3112121
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ShapeTest {
+
+	@Test
+    public void testCircle() {	
+		
+    }
+
+    @Test
+    public void testRhombus() {   	
+    }
+
+    @Test
+    public void testRightAngledTriangle() {    	
+    }
+
+    @Test
+    public void testIntegration() {    	
+    }
+
+}
